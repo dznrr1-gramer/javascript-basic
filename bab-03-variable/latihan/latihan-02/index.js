2490e45abb0628c15d43c807b4fe366251e39fce
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 13:36:57 +0700

    mengerjakan latihan-02

---
+++ bab-03-variable/latihan/latihan-02/index.js
@@ -0,0 +1,7 @@
+const SaldoAwal = 10000;
+let jumlah = 5000;
+let SaldoAkhir = SaldoAwal + jumlah;
+
+console.log("Saldo Awal : " + SaldoAwal);
+console.log("Jumlah     : " + SaldoAwal + ' + ' + jumlah);
+console.log("Saldo Akhir: " + SaldoAkhir);
