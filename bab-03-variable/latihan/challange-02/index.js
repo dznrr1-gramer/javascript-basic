95b5e34d5e3fa4f1c1c2b45390902843747f94e4
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 14:10:01 +0700

    mengerjakan challange-02

---
+++ bab-03-variable/latihan/challange-02/index.js
@@ -0,0 +1,13 @@
+const namaLengkap = "Dzulfian Nur Raihan";
+const umur = 19;
+const kotaAsal = "Lumajang";
+const email = "dzulfian1nr@gmail.com";
+const hobi = "Keliling dunia";
+const citaCita = "Fullstack Developer";
+
+console.log("Nama Lengkap : " + namaLengkap);
+console.log("Umur : " + umur);
+console.log("Kota Asal : " + kotaAsal);
+console.log("Email : " + email);
+console.log("Hobi : " + hobi);
+console.log("Cita-cita : " + citaCita);
