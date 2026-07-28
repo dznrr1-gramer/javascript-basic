6185989eb90229b2fecef244ae7278441fcd41df
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 13:51:35 +0700

    mengerjakan latihan-04

---
+++ bab-03-variable/latihan/latihan-04/index.js
@@ -0,0 +1,8 @@
+const nama = 'Dzulfian Nur Raihan';
+const tahunLahir = 2007;
+let tahunSekarang = 2026;
+
+console.log(nama); 
+console.log("Tahun Lahir : " + tahunLahir);
+console.log("Tahun Sekarang : " + tahunSekarang);
+console.log("Umur : " + (tahunSekarang - tahunLahir));
