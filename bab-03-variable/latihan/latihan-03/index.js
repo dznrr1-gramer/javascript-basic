5692adf45add11ef0136fbb6899d34d6329d6c41
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 13:40:20 +0700

    mengerjakan latihan-03

---
+++ bab-03-variable/latihan/latihan-03/index.js
@@ -0,0 +1,9 @@
+const Produk = "Buku"; // Produk : Buku
+let Harga = 10000; // Harga : 10000
+let Jumlah = 3; // Jumlah : 3
+const Total = Harga * Jumlah; // Total : 30000
+
+console.log("Produk : " + Produk);
+console.log("Harga : " + Harga);
+console.log("Jumlah : " + Jumlah);
+console.log("Total : " + Total);
