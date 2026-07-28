b77474182aee3149706ffb0ea98abdf904f5ff9c
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 13:35:40 +0700

    mengerjakan latihan-01

---
+++ bab-03-variable/latihan/latihan-01/index.js
@@ -0,0 +1,11 @@
+const biodata = '===== BIODATA =====';
+const nama = 'Nama : Dzulfian Nur Raihan';
+const umur = 'Umur : 19';
+const kota = 'Kota : Yogyakarta';
+const pekerjaan = 'Pekerjaan : freelance';
+
+console.log(biodata);
+console.log(nama);
+console.log(umur);
+console.log(kota);
+console.log(pekerjaan);
