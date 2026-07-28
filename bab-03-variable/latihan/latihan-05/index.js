aaa9b7cf7189e28b1dd447a55dd4114e84fc69f9
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-28 13:57:30 +0700

    mengerjakan latihan-05

---
+++ bab-03-variable/latihan/latihan-05/index.js
@@ -0,0 +1,9 @@
+let nilaiMatematika = 80;
+let nilaiBahasaIndonesia = 90;
+let nilaiIPA = 70;
+const rataRata = ((nilaiMatematika + nilaiBahasaIndonesia + nilaiIPA) / 3);
+
+console.log("Nilai Matematika : " + nilaiMatematika);
+console.log("Nilai Bahasa Indonesia : " + nilaiBahasaIndonesia);
+console.log("Nilai IPA : " + nilaiIPA);
+console.log("Rata-rata nilai : " + rataRata);
