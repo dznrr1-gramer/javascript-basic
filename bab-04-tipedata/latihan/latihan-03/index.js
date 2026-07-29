26214860bf7db6cbb2595d68170a5a3bd01008cd
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-29 11:34:35 +0700

    mengerjakan latihan-03

---
+++ bab-04-tipedata/latihan/latihan-03/index.js
@@ -0,0 +1,15 @@
+const profil = {
+    nama: "Dzulfian Nur Raihan",
+    umur: 19,
+    alamat: "Jl. Mayor Kmari Sampurno Gg.2/Dosen No.18 , Lumajang, Jawa Timur",
+    sekolah: "Pondok IT",
+    jurusan: "Teknik Informatika",
+    aktif: true
+};
+
+console.log("Nama : " + (profil.nama)); // Output: Tampil nama saja
+console.log("Umur : " + (profil.umur)); // Output: Tampil umur saja
+console.log("Alamat : " + (profil.alamat)); // Output: Tampil alamat saja
+console.log("Sekolah : " + (profil.sekolah)); // Output: Tampil sekolah saja
+console.log("Jurusan : " + (profil.jurusan)); // Output: Tampil jurusan saja
+console.log("Aktif : " + (profil.aktif)); // Output: Tampil aktif saja
