9ac0254bf528c520be0f0eaac792f3fbdeb9a9df
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-29 13:47:35 +0700

    mengerjakan latihan-05

---
+++ bab-04-tipedata/latihan/latihan-05/index.js
@@ -0,0 +1,7 @@
+console.log(typeof "Hello");// Output: string
+console.log(typeof 100);// Output: number
+console.log(typeof true);// Output: boolean
+console.log(typeof null);// Output: null
+console.log(typeof []);// Output: array
+console.log(typeof {});// Output: object
+console.log(typeof undefined);// Output: undefined
