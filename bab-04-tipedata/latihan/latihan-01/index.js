10f45cd50d3f060eefd9a83b1f7ee74e43793254
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-29 10:56:42 +0700

    mengerjakan latihan-01

---
+++ bab-04-tipedata/latihan/latihan-01/index.js
@@ -0,0 +1,11 @@
+let namaSiswa = "Dzulfian Nur Raihan";
+let umurSiswa = 19;
+let sudahLulus = true;
+let alamat = null;
+let nomerTelpon;
+
+console.log(namaSiswa); // Output: Dzulfian Nur Raihan
+console.log(umurSiswa); // Output: 19
+console.log(sudahLulus); // Output: true
+console.log(alamat); // Output: null 
+console.log(nomerTelpon); // Output: undefined
