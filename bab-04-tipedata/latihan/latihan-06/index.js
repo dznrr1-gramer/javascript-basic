7879090be6bd2dddf98c539bca3f171d611d0351
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-29 13:53:12 +0700

    mengerjakan latihan-06

---
+++ bab-04-tipedata/latihan/latihan-06/index.js
@@ -0,0 +1,16 @@
+const tahun = "2026";
+const umur = 17;
+const aktif = true;
+
+console.log(typeof tahun);
+console.log(typeof umur);
+console.log(typeof aktif);
+
+console.log(tahun + 1);
+console.log(umur + 1);
+
+// 1. Apa tipe data variabel `tahun`? string
+// 2. Mengapa hasil `tahun + 1` bukan menjadi `2027`? karnya tahun bertipe string bukan number
+// 3. Apa tipe data variabel `umur`? number
+// 4. Berapa hasil `umur + 1`? 18
+// 5. Apa fungsi operator `typeof`? untuk mengecek tipe data
