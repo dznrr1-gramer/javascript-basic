82aca3b4f22ecbe6b2ce1aca414cdaf51b8ac11a
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 11:20:48 +0700

    mengerjakan latihan-11

---
+++ bab-05-operator/latihan/latihan-11/index.js
@@ -0,0 +1,14 @@
+let angka = 20;
+
+angka + 5;
+
+console.log(angka);
+
+// Output: 20
+// `angka` tetap sama dengan 20 karena angka + 5 itu hanya menambahkan tapi tidak menyimpan
+
+angka += 5;
+
+console.log(angka);
+
+// Output: 25
