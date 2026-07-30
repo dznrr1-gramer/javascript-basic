5dadf4f82483abf104595db11dd0f6ec012c68e0
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 10:56:21 +0700

    mengerjakan latihan-06

---
+++ bab-05-operator/latihan/latihan-06/index.js
@@ -0,0 +1,6 @@
+console.log(10 > 5); // Output: true
+console.log(10 < 5); // Output: false
+console.log(10 >= 10); // Output: true
+console.log(5 <= 3); // Output: false
+console.log(8 != 8); // Output: false
+console.log(12 !== "12"); // Output: true
