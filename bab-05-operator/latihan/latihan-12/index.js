b954d9ac423416a1650fb323a403bcb4698a3368
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 11:22:27 +0700

    mengerjakan latihan-12

---
+++ bab-05-operator/latihan/latihan-12/index.js
@@ -0,0 +1,8 @@
+let umur = "18";
+
+console.log(umur > 17); // true
+console.log(umur === 18); // false
+
+// penjelasan kenapa hasilnya berbeda
+// "18" > 17 -> true karena angka 18 dianggap sama dengan string "18"
+// "18" === 18 -> false karena angka 18 dianggap sama dengan string "18" tetapi tipe datanya berbeda
