5bd6e41d142297513ba9db5fccbfce5f6ff8e57a
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 11:26:44 +0700

    mengerjakan latihan-14

---
+++ bab-05-operator/latihan/latihan-14/index.js
@@ -0,0 +1,10 @@
+const hargaBuku = 45000;
+const jumlahBeli = 4;
+const diskon = 15000;
+
+const subtotal = hargaBuku * jumlahBeli;
+const total = subtotal - diskon;
+
+console.log("Subtotal : " + subtotal);
+console.log("Diskon : " + diskon);
+console.log("Total : " + total);
