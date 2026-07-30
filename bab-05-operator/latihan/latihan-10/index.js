8bf88e3a782ee4d692cb57caaf94ae2c226ef2fe
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 11:11:53 +0700

    mengerjakan latihan-10

---
+++ bab-05-operator/latihan/latihan-10/index.js
@@ -0,0 +1,8 @@
+console.log(8 + 4 * 2); // Output: 16
+console.log((8 + 4) * 2); // Output: 24
+console.log(20 / 5 + 3 * 2); // Output: 10
+
+// penjelasan kenapa hasilnya berbeda
+// 8 + 4 * 2 -> 16 karena perkalian dilakukan terlebih dahulu lalu dijumlahkan
+// (8 + 4) * 2 -> 24 karena yang di dalam kurung dilakukan terlebih dahulu lalu dikalikan
+// 20 / 5 + 3 * 2 -> 10 karena pembagian dan perkalian dilakukan terlebih dahulu lalu dijumlahkan
