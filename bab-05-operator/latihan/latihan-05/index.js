5176136026381f5c665dff4dd12ff54f26543b8d
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-30 10:53:51 +0700

    mengerjakan latihan-05

---
+++ bab-05-operator/latihan/latihan-05/index.js
@@ -0,0 +1,10 @@
+const harga = 18000;
+const jumlah = 6;
+const diskon = 10000;
+
+const subtotal = harga * jumlah;
+const total = subtotal - diskon;
+
+console.log("Subtotal : " + subtotal);
+console.log("Diskon : " + diskon);
+console.log("Total : " + total);
