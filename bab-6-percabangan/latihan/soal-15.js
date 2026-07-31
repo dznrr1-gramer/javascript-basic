9c24639ea1bcafc48c557c81701efb9cdcdbf394
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 11:14:00 +0700

    menjawab soal-15

---
+++ bab-6-percabangan/latihan/soal-15.js
@@ -0,0 +1,8 @@
+let username = "admin";
+let password = "12345";
+
+if (username === "admin" && password === "12345") {
+    console.log("Selamat Datang");
+} else {
+    console.log("Username atau password salah");
+}
