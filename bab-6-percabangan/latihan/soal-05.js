eb8d853c14229ccd6ceaabe358f2aec2af611b3e
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 10:09:33 +0700

    menjawab soal-05

---
+++ bab-6-percabangan/latihan/soal-05.js
@@ -0,0 +1,19 @@
+let menu = "teh";
+
+let teh = 10000;
+let kopi = 15000;
+let jus = 18000;
+
+switch (menu) {
+    case "teh":
+        console.log("Teh :",teh);
+        break;
+    case "kopi":
+        console.log("Kopi :",kopi);
+        break;
+    case "susu":
+        console.log("Jus :",jus);
+        break;
+    default:
+        console.log("Menu tidak tersedia");
+}
