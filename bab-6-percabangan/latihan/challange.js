8b9058c69492758ed6dbf241d60d9f6b57287227
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 11:20:47 +0700

    menjawab challange

---
+++ bab-6-percabangan/latihan/challange.js
@@ -0,0 +1,13 @@
+let umur = 18;
+
+if (umur <= 5) {
+    console.log("kategori :","Balita");
+} else if (umur <= 12) {
+    console.log("kategori :","Anak-anak");
+} else if (umur <= 17) {
+    console.log("kategori :","Remaja");
+} else if (umur <= 59) {
+    console.log("kategori :","Dewasa");
+} else {
+    console.log("kategori :","Lansia");
+}
