6147bcdbb011fb281eb1cb25aacaf69fe758e979
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 11:09:44 +0700

    menjawab soal-13

---
+++ bab-6-percabangan/latihan/soal-13.js
@@ -0,0 +1,10 @@
+let nilai = 76;
+if (nilai >= 90) {
+    console.log("Sangat baik");
+} else if (nilai >= 75) {
+    console.log("baik");
+} else if (nilai >= 60) {
+    console.log("cukup");
+} else { (nilai < 60)
+    console.log("perlu belajar lagi");
+}
