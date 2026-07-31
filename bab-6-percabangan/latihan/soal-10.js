5250dbbb81bc3ebfbfaeb3e343c5fb27a67619b6
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 10:32:03 +0700

    menjawab soal-10

---
+++ bab-6-percabangan/latihan/soal-10.js
@@ -0,0 +1,11 @@
+let warna = "merah";
+
+if (warna === "merah") {
+    console.log("Berhenti");
+} else if (warna === "kuning") {
+    console.log("Hati-hati");
+} else if (warna === "hijau") {
+    console.log("Jalan");
+} else {
+    console.log("Warna lampu tidak dikenali");
+}
