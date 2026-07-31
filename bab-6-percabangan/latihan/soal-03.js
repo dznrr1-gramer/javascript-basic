8b4ba61de9cfb32db1d1b4f664aef641b8367dab
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 09:59:16 +0700

    menjawab soal-03

---
+++ bab-6-percabangan/latihan/soal-03.js
@@ -0,0 +1,12 @@
+let nilai = 87;
+if (nilai >= 90) {
+    console.log("Nilai Anda A");
+} else if (nilai >= 80) {
+    console.log("Nilai Anda B");
+} else if (nilai >= 70) {
+    console.log("Nilai Anda C");
+} else if (nilai >= 60) {
+    console.log("Nilai Anda D");
+} else {
+    console.log("Nilai Anda E");
+}
