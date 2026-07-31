bcd1d78869cb6a6b03aa486abcece48426110385
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-07-31 10:18:58 +0700

    menjawab soal-06

---
+++ bab-6-percabangan/latihan/soal-06.js
@@ -0,0 +1,7 @@
+if ("") {
+    console.log("A");// Tidak akan dieksekusi
+} else {
+    console.log("B");// Akan dieksekusi
+}
+
+// alasan kenapa yang "A" tidak dieksekusi dan "B" dieksekusi adalah karena nilai "" (string kosong) adalah Falsy
