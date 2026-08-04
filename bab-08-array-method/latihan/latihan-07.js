7b73ca464e355050e7b9856000ee3e08b05589e9
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-04 13:27:02 +0700

    mengerjakan latihan-07

---
+++ bab-08-array-method/latihan/latihan-07.js
@@ -0,0 +1,9 @@
+let antrian = [
+  "Budi",
+  "Ani",
+  "Joko",
+];
+let dilayani = antrian.shift();
+console.log("Sedang dilayani:", dilayani);
+antrian.unshift("Dewi");
+console.log("Antrian:", antrian);
