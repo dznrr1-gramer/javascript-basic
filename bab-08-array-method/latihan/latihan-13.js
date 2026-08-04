912621677fe78670339e1da512cc7be55e8b247b
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-04 13:50:13 +0700

    mengerjakan latihan-13

---
+++ bab-08-array-method/latihan/latihan-13.js
@@ -0,0 +1,9 @@
+let belanja = [];
+belanja.push("Beras");
+belanja.push("Minyak");
+belanja.push("Gula");
+belanja.pop();
+belanja.push("Telur");
+belanja.unshift("Tepung");
+console.log("Daftar belanja: " + belanja);
+console.log("Jumlah barang: " + belanja.length);
