a3b0aba8ac779917178d7f7db35e2cf9fb53f8ff
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-04 13:39:46 +0700

    mengerjakan latihan-10

---
+++ bab-08-array-method/latihan/latihan-10.js
@@ -0,0 +1,9 @@
+let buah = [
+  "Apel",
+  "Jeruk",
+  "Mangga",
+  "Anggur",
+];
+let buahTerpilih = buah.splice(1, 1, "Semangka");
+console.log("Buah yang dihapus: ", buahTerpilih);
+console.log("Buah yang tersedia: ", buah);
