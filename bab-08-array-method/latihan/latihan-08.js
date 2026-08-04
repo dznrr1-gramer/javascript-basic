484aa3cce9e03868bde15ef4c56f5e51a6b58f16
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-04 13:32:29 +0700

    mengerjakan latihan-08

---
+++ bab-08-array-method/latihan/latihan-08.js
@@ -0,0 +1,10 @@
+let kota = [
+  "Jakarta",
+  "Bandung",
+  "Yogyakarta",
+  "Surabaya",
+];
+
+console.log(kota.indexOf("Bandung")); 
+console.log(kota.indexOf("Surabaya"));
+console.log(kota.indexOf("Bali"));
