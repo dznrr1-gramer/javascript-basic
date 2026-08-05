66b9634f9bcdd6eec4d5138fa33394d282c2e32c
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-05 10:46:52 +0700

    mengerjakan  soal-3

---
+++ bab-09-fungsi/latihan/latihan-03.js
@@ -0,0 +1,24 @@
+function tampilkanProduk(namaProduk, harga, stok) {
+    console.log("Produk : " + namaProduk);
+    console.log("Harga  : Rp" + harga);
+    console.log("Stok   : " + stok);
+}
+
+tampilkanProduk("Mouse", 150000, 12);
+
+// - namaProduk
+// - harga
+// - stok
+
+// Contoh:
+
+// ```javascript
+// tampilkanProduk("Mouse", 150000, 12);
+// ```
+
+// Output:
+
+// ```
+// Produk : Mouse
+// Harga  : Rp150000
+// Stok   : 12
