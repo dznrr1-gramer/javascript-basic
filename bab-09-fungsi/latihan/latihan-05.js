a0ecad2a9fc91679f3320fce878023fad7335f5e
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-05 10:55:48 +0700

    mengerjakan  soal-5

--- bab-09-fungsi/latihan/latihan-05.js
+++ bab-09-fungsi/latihan/latihan-05.js
@@ -3,17 +3,4 @@ function jumlah(a, b) {
 }
 
 let hasil = jumlah(8, 12);
-console.log(hasil);
-
-// Buat function bernama `jumlah()`.
-
-// Function menerima dua angka.
-
-// Kemudian mengembalikan hasil penjumlahannya menggunakan `return`.
-
-// Contoh:
-
-// ```javascript
-// let hasil = jumlah(8, 12);
-
-// console.log(hasil);
+console.log(hasil); 
