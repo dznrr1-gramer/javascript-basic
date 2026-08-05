171af792dd6b4b46b12237ea01b249d7932b3a9a
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-05 11:50:36 +0700

    mengerjakan  soal-11

---
+++ bab-09-fungsi/latihan/latihan-11.js
@@ -0,0 +1,9 @@
+function biodata(nama, umur, pekerjaan) {
+    console.log("======= BIODATA =======");
+    console.log("Nama: " + nama);
+    console.log("Umur: " + umur);
+    console.log("Pekerjaan: " + pekerjaan);
+    console.log("=======================");
+}
+
+biodata("John Doe", 30, "Programmer");
