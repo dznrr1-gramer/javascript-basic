97827e3eafe83e6bbc74b12483450cb024f78f63
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-05 11:04:53 +0700

    mengerjakan  soal-6

---
+++ bab-09-fungsi/latihan/latihan-06.js
@@ -0,0 +1,6 @@
+function luasPersegiPanjang(panjang, lebar) {
+    console.log("hasil dari luas persegi panjang " + panjang + " x " + lebar + " = " + (panjang * lebar));
+    return panjang * lebar;
+}
+
+let hasilLuas = luasPersegiPanjang(10, 5);
