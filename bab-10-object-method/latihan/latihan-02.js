2c8a35773e22b6287da8fbd2055ee0b29c40448c
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-06 10:57:21 +0700

    mengrjakan soal-02

---
+++ bab-10-object-method/latihan/latihan-02.js
@@ -0,0 +1,22 @@
+const buku = {
+  judul: "Belajar JavaScript",
+  penulis: "Andi",
+  harga: 120000,
+};
+
+console.log(buku.judul);
+console.log(buku.harga);
+// ```
+
+// Tampilkan:
+
+// - Judul buku
+// - Harga buku
+
+// Gunakan **Dot Notation**.
+
+// Output:
+
+// ```
+// Belajar JavaScript
+// 120000
