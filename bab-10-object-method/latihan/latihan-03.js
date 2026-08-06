1a13c6d29160aefb6429cdffb305db6540919a21
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-06 10:59:42 +0700

    mengrjakan soal-03

---
+++ bab-10-object-method/latihan/latihan-03.js
@@ -0,0 +1,19 @@
+const mobil = {
+  merek: "Toyota",
+  warna: "Putih",
+};
+
+mobil.warna = "Hitam";
+mobil.tahun = 2025;
+
+console.log(mobil);
+// ```
+
+// Lakukan perubahan berikut.
+
+// - Ubah warna menjadi **Hitam**
+// - Tambahkan properti baru bernama `tahun` dengan nilai **2025**
+
+// Kemudian tampilkan object tersebut.
+
+// ---
