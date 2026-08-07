82960fbb99c11fbd0f56572b3820be56f99ca8a9
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 14:01:20 +0700

    mengerjakan pengerjaan bab-11 soal-14

--- bab-11-iterable-dan-non-iterable/latihan-14.js
+++ bab-11-iterable-dan-non-iterable/latihan-14.js
@@ -1,1 +1,12 @@
- 
+ const daftarBelanja = [
+  "Beras",
+  "Minyak",
+  "Gula",
+  "Telur",
+  "Sabun",
+];
+
+for (let item of daftarBelanja) {
+    const index = daftarBelanja.indexOf(item) + 1;
+    console.log(index, ". " ,item);
+}
