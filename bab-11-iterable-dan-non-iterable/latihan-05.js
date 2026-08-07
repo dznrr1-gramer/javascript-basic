57b6bdb2dc5f7b2ddba6e051060d9304b44f7bec
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 11:08:13 +0700

    mengerjakan pengerjaan bab-11 soal-05

--- bab-11-iterable-dan-non-iterable/latihan-05.js
+++ bab-11-iterable-dan-non-iterable/latihan-05.js
@@ -1,1 +1,11 @@
- 
+const nilai = [90, 80, 85, 100, 95];
+let total = 0;
+let average = 0;
+
+for (let n of nilai) {
+  total += n;
+}
+
+const rataRata = total / nilai.length;
+
+console.log("Rata-rata:", rataRata);
