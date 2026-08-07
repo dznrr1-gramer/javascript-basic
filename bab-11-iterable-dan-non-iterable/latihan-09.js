04770705214c781336831e70f413fa90bdd0be93
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 11:24:15 +0700

    mengerjakan pengerjaan bab-11 soal-09

--- bab-11-iterable-dan-non-iterable/latihan-09.js
+++ bab-11-iterable-dan-non-iterable/latihan-09.js
@@ -1,1 +1,10 @@
- 
+//  Tentukan perulangan yang paling tepat untuk data berikut.
+
+// | Data | Gunakan |
+// |------|---------|
+// | Array | `for...of` |
+// | String | `for...of` |
+// | Object | `for...in` |
+// | Object | `Object.keys()`, `Object.values()`, atau `Object.entries()` |
+
+// Jelaskan alasanmu.
