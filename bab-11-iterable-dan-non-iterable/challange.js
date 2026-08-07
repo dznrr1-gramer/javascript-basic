f659c357ee781bae7e07e35bcec7ccca0a18d764
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 14:07:43 +0700

    mengerjakan pengerjaan bab-11 challange

--- bab-11-iterable-dan-non-iterable/challange.js
+++ bab-11-iterable-dan-non-iterable/challange.js
@@ -1,1 +1,14 @@
- 
+ const kelas = {
+  nama: "JavaScript Dasar",
+  mentor: "Pak Budi",
+  jumlahSiswa: 20,
+};
+
+for (const key in kelas) {
+  console.log(key + ": " + kelas[key]);
+}
+
+kelas.ruangan = "Lab Komputer";
+for (const key in kelas) {
+  console.log(key + ": " + kelas[key]);
+}
