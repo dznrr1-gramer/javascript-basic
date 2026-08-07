37f74055041b414cf76962b68561f9f171ad685b
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 14:02:59 +0700

    mengerjakan pengerjaan bab-11 soal-15

--- bab-11-iterable-dan-non-iterable/latihan-15.js
+++ bab-11-iterable-dan-non-iterable/latihan-15.js
@@ -1,1 +1,10 @@
- 
+ const nilai = [75, 90, 88, 100, 67, 95];
+ let terbesar = 0;
+
+ for (let n of nilai) {
+    if (n > terbesar) {
+        terbesar = n;
+    }
+ }
+
+ console.log("Nilai tertinggi : " , terbesar);
