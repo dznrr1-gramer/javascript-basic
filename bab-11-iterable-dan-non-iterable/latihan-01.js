7a08b50b5eec39dc1f256b967a7eab9e6824a772
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 10:45:10 +0700

    mengerjakan pengerjaan bab-11 soal-01

---
+++ bab-11-iterable-dan-non-iterable/latihan-01.js
@@ -0,0 +1,18 @@
+
+
+// // Tentukan apakah tipe data berikut termasuk **Iterable** atau **Non-Iterable**.
+
+// // 1. `"Indonesia"`
+// // 2. `["Merah", "Putih"]`
+// // 3. `150`
+// // 4. `true`
+// // 5. `{ nama: "Budi" }`
+
+// // Tuliskan jawabanmu beserta alasannya.
+// // Jawaban:
+
+// 1. `"Indonesia"` - **Iterable**. String adalah tipe data yang dapat diiterasi, sehingga kita bisa menggunakan loop seperti `for...of` untuk mengakses setiap karakter dalam string.
+// 2. `["Merah", "Putih"]` - **Iterable**. Array adalah tipe data yang dapat diiterasi, sehingga kita bisa menggunakan loop seperti `for...of` untuk mengakses setiap elemen dalam array.
+// 3. `150` - **Non-Iterable**. Number adalah tipe data yang tidak dapat diiterasi, sehingga kita tidak bisa menggunakan loop seperti `for...of` untuk mengakses setiap karakter dalam number.
+// 4. `true` - **Non-Iterable**. Boolean adalah tipe data yang tidak dapat diiterasi, sehingga kita tidak bisa menggunakan loop seperti `for...of` untuk mengakses setiap karakter dalam boolean.
+// 5. `{ nama: "Budi" }` - **Non-Iterable**. Object adalah tipe data yang tidak dapat diiterasi, sehingga kita tidak bisa menggunakan loop seperti `for...of` untuk mengakses setiap properti dalam object.
