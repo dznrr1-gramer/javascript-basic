e35d466f8acd6ae032a59806c9827f239d487c81
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-07 13:54:45 +0700

    mengerjakan pengerjaan bab-11 soal-13

--- bab-11-iterable-dan-non-iterable/latihan-13.js
+++ bab-11-iterable-dan-non-iterable/latihan-13.js
@@ -1,1 +1,10 @@
- 
+ const biodata = {
+  nama: "Andi",
+  umur: 20,
+  pekerjaan: "Programmer",
+  kota: "Yogyakarta",
+};
+
+for (const key in biodata) {
+  console.log(key + ": " + biodata[key]);
+}
