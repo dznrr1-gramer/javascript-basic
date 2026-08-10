a96f50a51ab1c00cb06d16401e5bb6a010a11093
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-10 14:24:54 +0700

    mengerjakan soal-10

--- bab-12-string-number-built-in-function/latihan/soal-09.js
+++ bab-12-string-number-built-in-function/latihan/soal-09.js
@@ -6,19 +6,3 @@ console.log(kalimatBaru);
 
 const kalimatBaru2 = kalimat.replaceAll(kata, kataBaru);
 console.log(kalimatBaru2);
-
-// Diberikan:
-
-// ```javascript
-// const kalimat = "JavaScript itu seru. Saya suka JavaScript.";
-// ```
-
-// ### A.
-
-// Gunakan `.replace()` untuk mengganti kata `JavaScript` menjadi `TypeScript`.
-
-// ### B.
-
-// Gunakan `.replaceAll()` untuk mengganti seluruh kata `JavaScript` menjadi `TypeScript`.
-
-// Perhatikan perbedaan hasilnya.
---
+++ bab-12-string-number-built-in-function/latihan/soal-10.js
@@ -0,0 +1,6 @@
+const data = "Budi,Ani,Doni,Siti";
+
+const names = data.split(",");
+for (let name of names) {
+    console.log(name);
+}
