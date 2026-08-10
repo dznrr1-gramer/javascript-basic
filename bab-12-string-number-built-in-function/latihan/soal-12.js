9801ee9ef97f545910a17c2c925ba4626f78da92
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-10 14:30:06 +0700

    mengerjakan soal-12

---
+++ bab-12-string-number-built-in-function/latihan/soal-12.js
@@ -0,0 +1,6 @@
+console.log(parseInt("25.75"));// Output: 25
+console.log(parseFloat("25.75"));// Output: 25.75
+
+// penjelasan kenapa hasilnya berbeda
+// parseInt("25.75") -> 25 karena angka 25 dianggap sama dengan angka 25
+// parseFloat("25.75") -> 25.75 karena angka 25.75 dianggap sama dengan angka 25.75
