f7ebbe4c477bb2ba6799e45179b26e97a2c066c9
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-10 13:58:13 +0700

    mengerjakan soal-04

---
+++ bab-12-string-number-built-in-function/latihan/soal-04.js
@@ -0,0 +1,29 @@
+const pesan = "Selamat datang di kelas JavaScript!";
+
+pesan.includes("JavaScript")
+    ? console.log("Pesan membahas JavaScript")
+    : console.log("Pesan tidak membahas JavaScript");
+
+// Diberikan:
+
+// ```javascript
+// const pesan = "Selamat datang di kelas JavaScript!";
+// ```
+
+// Periksa apakah pesan tersebut mengandung kata:
+
+// ```text
+// JavaScript
+// ```
+
+// Jika ada:
+
+// ```text
+// Pesan membahas JavaScript
+// ```
+
+// Jika tidak:
+
+// ```text
+// Pesan tidak membahas JavaScript
+// ```
