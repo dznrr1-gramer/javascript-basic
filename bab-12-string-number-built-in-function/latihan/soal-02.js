733517d74cc88ecc91da39f15e2d0c2f77eae795
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-10 13:55:53 +0700

    mengerjakan soal-03

--- bab-12-string-number-built-in-function/latihan/soal-02.js
+++ bab-12-string-number-built-in-function/latihan/soal-02.js
@@ -1,25 +1,3 @@
 const email = "   Budi@Email.COM   ";
 
 console.log(email.trim().toLowerCase());
-
-// Diberikan input:
-
-// ```javascript
-// const email = "   Budi@Email.COM   ";
-// ```
-
-// Bersihkan input tersebut sehingga menghasilkan:
-
-// ```text
-// Budi@Email.COM
-// ```
-
-// Kemudian ubah menjadi huruf kecil.
-
-// Output akhir:
-
-// ```text
-// budi@email.com
-// ```
-
-// > Gunakan `.trim()` dan `.toLowerCase()`.
---
+++ bab-12-string-number-built-in-function/latihan/soal-03.js
@@ -0,0 +1,5 @@
+const username = "santri_jago_ngoding";
+
+username.length>=8
+    ? console.log("Username valid")
+    : console.log("Username terlalu pendek");
