77390766a184b1e4e7fcc4ed8d053bc98ea56a65
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-11 14:08:10 +0700

    mengerjakan bab-13 soal-01

---
+++ bab-13-scope-hoisting-closure/latihan/soal-01.js
@@ -0,0 +1,16 @@
+const nama = "Budi";
+
+function belajar() {
+    const bahasa = "JavaScript";
+
+    if (true) {
+        let level = "Pemula";
+        console.log(level);
+    }
+}
+
+belajar();
+
+// nama termasuk Global Scope.
+// bahasa termasuk Function Scope.
+// level termasuk Block Scope.
