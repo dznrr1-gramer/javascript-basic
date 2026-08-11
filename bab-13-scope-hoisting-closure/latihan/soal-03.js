b98d9489674aa193522c2bf0efeb9b40c59710c2
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-11 14:15:00 +0700

    mengerjakan bab-13 soal-03

---
+++ bab-13-scope-hoisting-closure/latihan/soal-03.js
@@ -0,0 +1,14 @@
+if (true) {
+  let pesan = "Halo Santri";
+
+  console.log(pesan);
+}
+
+console.log(pesan);
+
+// Output:
+// Halo Santri
+// ReferenceError: pesan is not defined
+
+// tidak berhasil 
+//Baris console.log(pesan) di luar blok if tidak dapat mengoperasikan variabel tersebut.
