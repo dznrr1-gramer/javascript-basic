a4566a4001cbeb9694f922ff2e0ec1185148daf0
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-11 17:44:42 +0700

    mengerjakan bab-13 soal-11

---
+++ bab-13-scope-hoisting-closure/latihan/soal-11.js
@@ -0,0 +1,10 @@
+// Deklarasi variabel terlebih dahulu sebelum pemanggilan fungsi
+const nama = "Budi";
+const umur = 17;
+
+function tampilkanData() {
+  console.log(nama);
+  console.log(umur);
+}
+
+tampilkanData();
