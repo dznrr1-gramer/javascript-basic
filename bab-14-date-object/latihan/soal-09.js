94955a55387217b7f141749cf4ee6803e9cf7f6b
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-13 13:54:12 +0700

    mengerjakan bab-14 soal-09

---
+++ bab-14-date-object/latihan/soal-09.js
@@ -0,0 +1,12 @@
+const tanggal = new Date(2026, 0, 1);
+
+    console.log(tanggal.getMonth());
+    console.log(tanggal.getDate());
+    console.log(tanggal.getFullYear());
+
+// Output:
+// 0
+// 1
+// 2026
+
+// grtMonth menghasilkan 0 karena urutan pemanggilan dimulai dari 0
