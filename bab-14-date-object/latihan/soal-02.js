0eb1451d4a03b8eb217e64934b400b35e9dcf439
Dzulfian Nur Raihan <dznrr1@gmail.com>
2026-08-13 11:36:30 +0700

    mengerjakan bab-14 soal-02

---
+++ bab-14-date-object/latihan/soal-02.js
@@ -0,0 +1,15 @@
+const sekarang = new Date();
+
+const tahun = sekarang.getFullYear();
+const bulan = sekarang.getMonth() + 1; // +1 karena indeks dimulai dari 0
+const tanggal = sekarang.getDate();
+const hari = sekarang.getDay();
+const jam = sekarang.getHours();
+const menit = sekarang.getMinutes();
+
+console.log("Tahun", tahun);
+console.log("Bulan", bulan);
+console.log("Tanggal", tanggal);
+console.log("Hari", hari);
+console.log("Jam", jam);
+console.log("Menit", menit);
